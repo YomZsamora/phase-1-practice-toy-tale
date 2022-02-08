8a6e4b3fec906dab9e8014a408ee5dd4e3990f1b
yomZsamora <samora.y@adzumi.co.ke>
2022-02-08 07:57:32 +0300

    Fetches the images using the url provided.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+	const addBtn = document.querySelector("#new-toy-btn");
+	const toyFormContainer = document.querySelector(".container");
+	addBtn.addEventListener("click", () => {
+		// hide & seek with the form
+		addToy = !addToy;
+		if (addToy) {
+			toyFormContainer.style.display = "block";
+		} else {
+			toyFormContainer.style.display = "none";
+		}
+	});
 });
+
+let fetchToys = () => {}
+
+// Fetches the images using the url provided.
+let fetchImages = () => {
+	fetch('http://localhost:3000/toys')
+	.then(resp => resp.json())
+   .then(toys => {
+      const imageURL = toys["image"]
+      const imageURLsArray = createImgElement(imageURL)
+      renderImgs(imageURLsArray)
+   })
+}
