46f8894476fce7b9f17d3bb81c51357d302f4553
yomZsamora <samora.y@adzumi.co.ke>
2022-02-08 08:57:13 +0300

    Done appending images from response in their respective cards using the retrieved image URL.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 let addToy = false;
 
-const toysCard = document.createElement("div");
-toysCard.className = "card";
-document.getElementById("toy-collection").appendChild(toysCard);
 
 document.addEventListener("DOMContentLoaded", () => {
-	const addBtn = document.querySelector("#new-toy-btn");
-	const toyFormContainer = document.querySelector(".container");
-	addBtn.addEventListener("click", () => {
-		// hide & seek with the form
-		addToy = !addToy;
-		if (addToy) {
-			toyFormContainer.style.display = "block";
-		} else {
-			toyFormContainer.style.display = "none";
-		}
-	});
+   
+   fetchImages();
+   const addBtn = document.querySelector("#new-toy-btn");
+   const toyFormContainer = document.querySelector(".container");
+   addBtn.addEventListener("click", () => {
+      // hide & seek with the form
+      addToy = !addToy;
+      if (addToy) {
+         toyFormContainer.style.display = "block";
+      } else {
+         toyFormContainer.style.display = "none";
+      }
+   });
 });
 
-let fetchToys = () => {}
+let fetchToys = () => {
+   
+}
 
 // Fetches the images using the url provided.
 let fetchImages = () => {
-	fetch('http://localhost:3000/toys')
-	.then(resp => resp.json())
+   fetch('http://localhost:3000/toys')
+   .then(resp => resp.json())
    .then(toys => {
-      const imageURL = toys["image"]
+      const imageURL = toys;
       const imageURLsArray = createImgElement(imageURL)
       renderImgs(imageURLsArray)
    })
@@ -34,12 +35,15 @@ let fetchImages = () => {
 // Creates img tag/element and adds the image URL to the src
 let createImgElement = imageURL => {
    return imageURL.map( image => {
-      return `<img src="${image}">`
+      return `<img src="${image.image}" class="toy-avatar">`
    })
 }
 
 let renderImgs = imageURLsArray => {
    imageURLsArray.forEach(url => {
+      const toysCard = document.createElement("div");
+      toysCard.className = "card";
+      document.getElementById("toy-collection").appendChild(toysCard);
       toysCard.innerHTML += url
    })
 }
