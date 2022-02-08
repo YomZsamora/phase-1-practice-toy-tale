db64338a82b74623ee320c697c41e88ba323109e
yomZsamora <samora.y@adzumi.co.ke>
2022-02-08 08:23:38 +0300

    Renders image element for each retrieved image.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 let addToy = false;
 
+const toysCard = document.createElement("div");
+toysCard.className = "card";
+document.getElementById("toy-collection").appendChild(toysCard);
+
 document.addEventListener("DOMContentLoaded", () => {
 	const addBtn = document.querySelector("#new-toy-btn");
 	const toyFormContainer = document.querySelector(".container");
@@ -27,8 +31,15 @@ let fetchImages = () => {
    })
 }
 
+// Creates img tag/element and adds the image URL to the src
 let createImgElement = imageURL => {
    return imageURL.map( image => {
       return `<img src="${image}">`
+   })
+}
+
+let renderImgs = imageURLsArray => {
+   imageURLsArray.forEach(url => {
+      toysCard.innerHTML += url
    })
 }
